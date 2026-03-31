9d04649d4bfe311bf28221641c04fce4ece97746
zibritar <richard.zibrita@mail.utoronto.ca>
2026-03-31 01:10:49 -0400

    Do the thing

---
+++ src/pages/csc309-e11/index.jsx
@@ -0,0 +1,8 @@
+export default function page() {
+	return (
+		<div id="auto-tester">
+			Hi. I am zibritar, and I have learned how to develop a full-stack web app
+			and deploy it!
+		</div>
+	)
+}
